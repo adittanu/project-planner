0dfb6eb1276656814e4125d26e94372d9e50349b
Adit Tanu <adiettanu27@gmail.com>
2022-12-23 10:53:17 +0700

    starter

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <RouterView />
+</template>
+
+<style>
+body {
+    background-attachment: #f2f2f2;
+}
+#app {
+    font-family: Arial, Helvetica, sans-serif;
+    -webkit-font-smoothing: antialiases;
+    -moz-osx-font-smoothing: grayscale;
+    max-width: 600px;
+    margin: 0 auto;
+    color: #555;
+}
+</style>
